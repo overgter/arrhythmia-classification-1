f679b77a980aa65bc154151478a1174df7288def
Ali Salah Alddin <rated.ali.7@gmail.com>
2018-04-04 11:21:40 +0200

    Add limit on number of examples per class

--- extract_features.m
+++ extract_features.m
@@ -1,22 +1,35 @@
-function [ samples, labels ] = extract_features(dataset, statistical_intervals)
+function [ samples, labels ] = extract_features(dataset, statistical_intervals, max_samples_per_class)
 %extract_features  Perform feature extraction and assign labels
 
 samples = [];
 labels = [];
+samples_per_class = zeros(12, 1);
 for r = 1:size(dataset.signals, 2)
     for s = 1:size(dataset.signals, 1)
         for b = 1:size(dataset.signals{s,r}, 1)
             %% Labels
             % Using binary classification (normal & abnormal)
-            num_classes = 2;
+            num_classes = 12;
             selected = 0;
             % class 1 : normal beats
-            subclasses{1} = {'N', 'L', 'R'};
-            subclasses{2} = {'A', 'a', 'J', 'S', 'V', 'E', 'F',  'e', 'j'};
+            subclasses{1} = {'N'};
+            subclasses{2} = {'L'};
+            subclasses{3} = {'R'};
+            subclasses{4} = {'A'};
+            subclasses{5} = {'a'};
+            subclasses{6} = {'J'};
+            subclasses{8} = {'S'};
+            subclasses{9} = {'j'};
+            subclasses{10} = {'V'};
+            subclasses{11} = {'E'};
+            subclasses{12} = {'F'};
             for c = 1:num_classes
                 if any(strcmp(dataset.classes{r}(b), subclasses{c}))
-                    labels = [labels; c];
-                    selected = 1;
+                    if samples_per_class(c) < max_samples_per_class
+                        labels = [labels; c];
+                        selected = 1;
+                        samples_per_class(c) = samples_per_class(c) + 1;
+                    end
                     break;
                 end
             end
@@ -34,6 +47,6 @@ for r = 1:size(dataset.signals, 2)
 end
 
 dataset_name = 'mitdb_dataset';
-dataset_path = [dataset_name '_interv' num2str(statistical_intervals) '.mat'];
+dataset_path = [dataset_name '_interv' num2str(statistical_intervals) '_max_per_class' num2str(max_samples_per_class) '.mat'];
 save(dataset_path, 'samples', 'labels');
 end
